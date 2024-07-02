1c9c75860c86b260bc2ad639459db490619b3131
Ozan Ekici <ozanekici@kale.com.tr>
2024-07-02 14:31:59 +0300

    DTos Added

---
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.ServiceDto;
+
+namespace HotelProject.WebUI.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ResultServiceDto, Service>().ReverseMap(); ;
+        }
+    }
+}
