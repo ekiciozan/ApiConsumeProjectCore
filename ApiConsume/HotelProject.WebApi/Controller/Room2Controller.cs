616e4a61b76d80aa7db7d33784053173590c1879
Ozan Ekici <ozanekici@kale.com.tr>
2024-07-02 11:29:40 +0300

    Added Automapper

---
+++ ApiConsume/HotelProject.WebApi/Controller/Room2Controller.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using HotelProject.BussinessLayer.Abstract;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room2Controller : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+        public Room2Controller(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDto roomAddDto)
+
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomAddDto); //Room'a roomAddDto yu at.
+            _roomService.TInsert(values);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(RoomUpdateDto roomUpdateDto)
+
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomUpdateDto); //Room'a roomAddDto yu at.
+            _roomService.TUpdate(values);
+            return Ok("Başarılı ile Güncellendi");
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+                CreateMap<RoomAddDto , Room> ();
+                CreateMap<Room, RoomAddDto> ();
+
+                CreateMap<RoomUpdateDto, Room> ().ReverseMap();
+            // DTO sınıfına geçilen proplar ile entity arasında eşleşmiş olacak.
+        }
+    }
+}
