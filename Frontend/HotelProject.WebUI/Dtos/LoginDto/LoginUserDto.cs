7f65171f0a55777eec723b1725b7da2b5c7034d6
Ozan Ekici <ekiciozann@gmail.com>
2024-07-04 13:55:39 +0300

    Login İşlemleri

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage = "Kullanıcı adı giriniz.")]
+        public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "Şifreyi giriniz.")]
+        public string Password { get; set; }
+    }
+}
